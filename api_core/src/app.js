be1fefeea5c0ecd944816eee3162da37bef49739
alikahwaji <alikahwaji.kahwaji@hotmail.com>
2017-08-08 21:48:19 +1200

    setting up the app.js file

--- api_core/src/app.js
+++ api_core/src/app.js
@@ -1,1 +1,12 @@
+import http from 'http'
 import express from 'express'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+
+import config from './config'
+import routes from './routes'
+
+let app =express
+app.server = http.createServer(app)
+
+

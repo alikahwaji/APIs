564b4369d8288654af6c9f82962c9d086b65c45e
alikahwaji <alikahwaji.kahwaji@hotmail.com>
2017-08-08 19:48:27 +1200

    Commiting JS re-cap

---
+++ JS/subJS/export.js
@@ -0,0 +1,14 @@
+export let myVariable = 942
+export let func1 = () => {
+    console.log('Hello from func1')
+}
+
+export let func2 = () => {
+    console.log('Hello from func2')
+}
+
+// module.exports = {
+//     myVariable,
+//     func1,
+//     func2
+// }
---
+++ JS/subJS/requireAndimport.js
@@ -0,0 +1,7 @@
+// let myFuncs = require ('./export.js')
+
+import {myVariable, func1, func2} from './export'
+
+console.log(`The value of myVar is ${myVariable}`)
+func1()
+func2()

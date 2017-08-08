f97353ee44f3c48110f26a80c8dea49b6b331ba7
alikahwaji <alikahwaji.kahwaji@hotmail.com>
2017-08-08 22:33:10 +1200

    Setting MongoDB

--- api_core/src/index.js
+++ api_core/src/index.js
@@ -9,4 +9,15 @@ import routes from './routes'
 let app =express
 app.server = http.createServer(app)
 
+//middleware
+
+//conifg
+
+
+//api router v1
+app.use('/v1',routes)
+app.server.listen(config,port)
+console.log(`Started on port ${app.server.address().port}`)
+
+export default app
 

900fdb3b061597d7447b16e02872dc077b303f39
alikahwaji <alikahwaji.kahwaji@hotmail.com>
2017-08-08 13:35:21 +1200

    Commiting modified modules plus adding some JS re-cap

---
+++ JS/index.js
@@ -0,0 +1,103 @@
+// var 1variable ='Ali' error
+// var var ='Ali'  error
+
+// console.log(`S{firstName} ${lastName}` )
+
+let a = 1
+console.log(a++) // prints the value 1 THEN  increments by 1
+console.log(a)
+console.log(++a)// increments THEN prints the value 
+
+let total =32+46 -13 
+console.log(total)
+let product =5*5
+console.log(product)
+
+console.log(3=='3') //true - checks for equal value 
+
+console.log(3==='3')// false - checks for equal value AND type (Strict Equality)
+
+let firstName = 'Ali'
+let lastName = 'Kahwaji'
+console.log(firstName +' '+ lastName)
+
+console.log(25%5)
+console.log(23%4)
+console.log('Welcome back')
+
+ a = 9
+let b = 32
+
+if (a > b) {
+console.log('a is greater than b')
+}else if (a === b ) {
+console.log('a is = to b')
+} else {
+console.log('a is less than b')
+}
+
+if ((a+b) > (a*b-248)) {
+    console.log('The condition is true')
+} else {
+    console.log('The condition is false')
+}
+
+if ((true) && (true)) {
+    console.log('true')
+} else {
+    console.log('false')
+}
+
+if ((false) || (false)) {
+    console.log('true')
+} else {
+    console.log('false')
+}
+
+let isDarkOutside = true 
+
+if(isDarkOutside) {
+    console.log('it is dark')
+} else {
+    console.log('it is daylight')
+}
+
+let color = 'black'
+switch (color) {
+    case 'yellow' :
+    console.log('Yellow')
+    break
+
+    case 'green' :
+    console.log('Green')
+    break
+
+    case 'red' :
+    console.log('Red')
+    break
+
+    default:
+    console.log('we can not find the color')
+
+   
+
+}
+
+ console.log('While loop')
+    let count = 5
+    while(count <= 10){
+        console.log(`The value is ${count++}`)
+        
+    }
+
+    console.log('Do while loop')
+    count = 5
+    do {
+        console.log(`The value is ${count}`)
+        count++
+    } while (count <= 10)
+
+    console.log ('For loop')
+    for (let x=0; x<= 10; x++) {
+        console.log(`The value is ${x}`)
+    }

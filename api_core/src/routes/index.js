e2889781e569fb737542b8748d482104b4c44e70
alikahwaji <alikahwaji.kahwaji@hotmail.com>
2017-08-11 10:33:08 +1200

    Modifing the router to wine router

--- api_core/src/routes/index.js
+++ api_core/src/routes/index.js
@@ -2,7 +2,7 @@ import express from 'express'
 import config from '../config'
 import middleware from '../middleware'
 import initializeDb from '../db'
-import coffee from '../controller/coffee'
+import coffee from '../controller/wine'
 
 let router = express()
 
@@ -14,7 +14,7 @@ initializeDb (db =>{
     router.use(middleware({config,db}))
 
     //api routes v1 
-    router.use('/coffee',coffee({config, db}))
+    router.use('/wine',coffee({config, db}))
 })
 
 export default router
